73892bb5fb261622869c8e031f598bf4b4e02b9f
linxiaopeng <linxiaopeng2013@gail.com>
2018-08-02 17:17:11 +0800

    #344

---
+++ LeetCode_String_344_reverseString.js
@@ -0,0 +1,14 @@
+// LeetCode 344. Reverse String
+
+// Write a function that takes a string as input and returns the string reversed.
+// Example: Given s = "hello", return "olleh".
+
+function reverseString(str) {
+    var j = '';
+    for (i = str.length; i > 0; i--) {
+        j = j + str.slice(0, i).slice(-1);
+    }
+    return j;
+}
+
+reverseString("Java");
