80f8e29d19cff81ec6eb165bd871b768bb6d9408
Linxiaopeng <linxiaopeng2013@gmail.com>
2018-08-07 03:37:08 +0800

    #345

--- 345_Reverse_Vowels_Of_A_String.js
+++ 345_Reverse_Vowels_Of_A_String.js
@@ -6,3 +6,25 @@
 // Example 2: Given s = "leetcode", return "leotcede".
 // Note: The vowels does not include the letter "y".
 
+function reverseVowels(s) {
+    var vowels = [];
+
+    for (i = 0; i < s.length; i++) {
+        if ((/^[aeiou]$/i).test(s[i])) {
+            vowels.push(s[i]);
+        }
+    }
+
+    var v = vowels.length - 1;
+    var sAry = s.split("");
+
+    for (var j = 0; j < sAry.length; j++) {
+        if ((/^[aeiou]$/i).test(sAry[j])) {
+            sAry[j] = vowels[v--];
+        }
+    }
+
+    return sAry.join("");
+}
+
+reverseVowels("hello");
