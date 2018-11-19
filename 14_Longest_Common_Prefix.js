afa07c533779d7c82266731def2d1e05d6cbe6e9
linxiaopeng <linxiaopeng2013@gmail.com>
2018-11-19 15:21:48 +0800

    #14

---
+++ 14_Longest_Common_Prefix.js
@@ -0,0 +1,24 @@
+// LeetCode 14. Longest Common Prefix
+
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+function longestCommonPrefix(s) {
+    if (s == null || s.length == 0) return "";
+
+    var same = s[0];
+    for (var i = 1; i < s.length; i++) {
+        var str = s[i];
+
+        var j = 0;
+        for (; j < same.length; j++) {
+            if (same[j] != str.charAt(j)) {
+                break;
+            }
+        }
+
+        same = same.slice(0, j);
+    }
+    return same;
+}
+
+longestCommonPrefix(["abcd", "abccc", "abcde"]);
