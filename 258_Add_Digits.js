ec6b865a7ac66973be4877d0f42ac698bcccfe5a
linxiaopeng <linxiaopeng2013@gmail.com>
2018-11-30 16:58:20 +0800

    #258

---
+++ 258_Add_Digits.js
@@ -0,0 +1,20 @@
+// LeetCode 258. Add Digits
+
+// Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+// For example:
+// Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+// Follow up:
+// Could you do it without any loop/recursion in O(1) runtime?
+
+function addDigits(num) {
+    while (num > 10) {
+        var sum = 0;
+        ("" + num).split("").forEach(function(v) {
+            sum += parseInt(v);
+        });
+        num = sum;
+    }
+    return num;
+}
+
+addDigits(138);
