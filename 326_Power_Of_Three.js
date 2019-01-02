70e4e8b80d77effe0d38779e68a07c2aceeb0b13
linxiaopeng <linxiaopeng2013@gmail.com>
2019-01-02 16:18:51 +0800

    #326

---
+++ 326_Power_Of_Three.js
@@ -0,0 +1,14 @@
+// LeetCode 326. Power of Three
+
+// Given an integer, write a function to determine if it is a power of three.
+// Follow up: Could you do it without using any loop / recursion?
+
+function isPowerOfThree(n) {
+    while (n > 2) {
+        if (n % 3 !== 0) return false;
+        n = parseInt(n / 3);
+    }
+    return n == 1;
+}
+
+isPowerOfThree(9);
